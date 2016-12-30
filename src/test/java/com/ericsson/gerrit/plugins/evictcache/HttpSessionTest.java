9a53ff30067aef10859c9a22b4b6d99ebc2e8ab4
Hector Oswaldo Caballero <hector.caballero@ericsson.com>
2016-12-29 22:12:33 -0500

    Replace EasyMock with Mockito
    Change-Id: I96b345e38c7d130ecf5469e24629503c640b12cd

--- src/test/java/com/ericsson/gerrit/plugins/evictcache/HttpSessionTest.java
+++ src/test/java/com/ericsson/gerrit/plugins/evictcache/HttpSessionTest.java
@@ -19,22 +19,24 @@ import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.google.common.truth.Truth.assertThat;
-import static org.easymock.EasyMock.expect;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.ericsson.gerrit.plugins.evictcache.CacheResponseHandler.CacheResult;
 import com.github.tomakehurst.wiremock.http.Fault;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.github.tomakehurst.wiremock.stubbing.Scenario;
 
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.easymock.EasyMockSupport;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.net.SocketTimeoutException;
 
-public class HttpSessionTest extends EasyMockSupport {
+@RunWith(MockitoJUnitRunner.class)
+public class HttpSessionTest {
   private static final int MAX_TRIES = 3;
   private static final int RETRY_INTERVAL = 250;
   private static final int TIMEOUT = 500;
@@ -52,8 +54,6 @@ public class HttpSessionTest extends EasyMockSupport {
   private static final String RETRY_AT_ERROR = "Retry at error";
   private static final String RETRY_AT_DELAY = "Retry at delay";
 
-  private Configuration cfg;
-  private CloseableHttpClient httpClient;
   private HttpSession httpSession;
 
   @Rule
@@ -61,17 +61,17 @@ public class HttpSessionTest extends EasyMockSupport {
 
   @Before
   public void setUp() throws Exception {
-    cfg = createMock(Configuration.class);
-    expect(cfg.getUrl()).andReturn(Constants.URL).anyTimes();
-    expect(cfg.getUser()).andReturn("user");
-    expect(cfg.getPassword()).andReturn("pass");
-    expect(cfg.getMaxTries()).andReturn(MAX_TRIES).anyTimes();
-    expect(cfg.getConnectionTimeout()).andReturn(TIMEOUT).anyTimes();
-    expect(cfg.getSocketTimeout()).andReturn(TIMEOUT).anyTimes();
-    expect(cfg.getRetryInterval()).andReturn(RETRY_INTERVAL).anyTimes();
-    replayAll();
-    httpClient = new HttpClientProvider(cfg).get();
-    httpSession = new HttpSession(httpClient, Constants.URL);
+    Configuration cfg = mock(Configuration.class);
+    when(cfg.getUrl()).thenReturn(Constants.URL);
+    when(cfg.getUser()).thenReturn("user");
+    when(cfg.getPassword()).thenReturn("pass");
+    when(cfg.getMaxTries()).thenReturn(MAX_TRIES);
+    when(cfg.getConnectionTimeout()).thenReturn(TIMEOUT);
+    when(cfg.getSocketTimeout()).thenReturn(TIMEOUT);
+    when(cfg.getRetryInterval()).thenReturn(RETRY_INTERVAL);
+
+    httpSession =
+        new HttpSession(new HttpClientProvider(cfg).get(), Constants.URL);
   }
 
   @Test
@@ -82,24 +82,24 @@ public class HttpSessionTest extends EasyMockSupport {
 
   @Test
   public void testNotAuthorized() throws Exception {
-    String expected = "unauthorized";
+    String whened = "unauthorized";
     stubFor(post(urlEqualTo(ENDPOINT)).willReturn(
-        aResponse().withStatus(UNAUTHORIZED).withBody(expected)));
+        aResponse().withStatus(UNAUTHORIZED).withBody(whened)));
 
     CacheResult result = httpSession.post(ENDPOINT, EMPTYJSON);
     assertThat(result.isSuccessful()).isFalse();
-    assertThat(result.getMessage()).isEqualTo(expected);
+    assertThat(result.getMessage()).isEqualTo(whened);
   }
 
   @Test
   public void testNotFound() throws Exception {
-    String expected = "not found";
+    String whened = "not found";
     stubFor(post(urlEqualTo(ENDPOINT)).willReturn(
-        aResponse().withStatus(NOT_FOUND).withBody(expected)));
+        aResponse().withStatus(NOT_FOUND).withBody(whened)));
 
     CacheResult result = httpSession.post(ENDPOINT, EMPTYJSON);
     assertThat(result.isSuccessful()).isFalse();
-    assertThat(result.getMessage()).isEqualTo(expected);
+    assertThat(result.getMessage()).isEqualTo(whened);
   }
 
   @Test
